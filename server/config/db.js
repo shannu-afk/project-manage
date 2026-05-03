7911c072ab38ec1def7f7a33ff2632ffb8e58d1d
k.shanmukh <kodalishannu666@gmail.com>
2026-05-03 11:15:51 +0530

    Log connection success message for MongoDB

--- server/config/db.js
+++ server/config/db.js
@@ -2,6 +2,7 @@ import mongoose from "mongoose";
 
 const connectDB = async () => {
   await mongoose.connect(process.env.MONGO_URI);
+  console.log("✅ Connected to MongoDB");
 };
 
 export default connectDB;
